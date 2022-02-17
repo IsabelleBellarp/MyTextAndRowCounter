89cccbcefd50a3e710075b1246045fc77c9796bf
IsabelleBellarp <isabellebellarp@gmail.com>
2022-02-17 09:22:32 +0100

    Code 1

---
+++ src/MångaÖvningarJava/Övning10.java
@@ -0,0 +1,28 @@
+package MångaÖvningarJava;
+
+import java.util.Scanner;
+
+public class Övning10 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Vad är det för tempratur ute?");
+        int F = scan.nextInt();
+
+        double C = (F-32) * 5 / 9;
+
+        System.out.println("Fahrenheit: " + F);
+        System.out.println();
+System.out.println();
+        //Hej
+
+
+
+
+
+
+        // C = F-32/1.8000
+    }
+}
